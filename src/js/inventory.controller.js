33e04b606186bda71031af50cbe3a08437de783b
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-03-29 16:32:37 -0400

    modified for service

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -3,34 +3,22 @@
 
   angular.module('inventory')
   .controller('InventoryController', InventoryController);
-  /**
-   * Constructs an Inventory Controller
-   */
-  function InventoryController(){
-    let vm = this;
 
+  InventoryController.$inject = ['ItemService'];
+  /**
+  * Constructs an Inventory Controller
+  */
+  function InventoryController(ItemService){
+    let vm = this;
     vm.newItem = {};
-    vm.inventory = [
-      { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
-      { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
-      { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
-      { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
-      { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
-      { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
-      { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
-      { "id": 46, "name": "guitar", "price": 899, "quantity": 0, "color": "blue", "discount": 150 },
-      { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
-      { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
-      { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
-      { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
-    ];
+    vm.inventory = ItemService.getAllItems();
 
     vm.tax = 0.0575;
     /**
-     * Determines final price of items with discounts and tax included
-     * @param  {Object} item tax and discount values
-     * @return {Number}     Final price
-     */
+    * Determines final price of items with discounts and tax included
+    * @param  {Object} item tax and discount values
+    * @return {Number}     Final price
+    */
     vm.totalPrice = function totalPrice(item){
       let price = item.price - item.discount;
       let tax = price * vm.tax;
@@ -39,25 +27,13 @@
     };
 
     /**
-     * Adds new items to the table
-     * @param  {Object} item Should have 'name', 'price', 'quantity', 'color', 'discount'
-     * @return {Void}
-     */
+    * Adds new items to the inventory array
+    * @param  {Object} item Should have 'name', 'price', 'quantity', 'color', 'discount'
+    * @return {Void}
+    */
     vm.addItem = function addItem(item) {
-      console.log(item);
-      if (typeof(item) !== 'object' || typeof(item.name) !== 'string'
-          || typeof(item.price) !== 'number' || typeof(item.quantity) !== 'number'
-          || typeof(item.color) !== 'string' || typeof(item.discount) !== 'number') {
-          return;
-      }
-      vm.inventory.push({
-        name: item.name,
-        price: item.price,
-        quantity: item.quantity,
-        color: item.color,
-        discount: item.discount
-      });
-
+      console.log('in controller addItem:', item);
+      ItemService.addItem(item);
       vm.newItem = {};
     };
 
